fbf321996b65f7e396a3f62df579e84157bedeb6
Mike Chinn <yournamehere2009@gmail.com>
2016-07-25 10:45:20 -0700

    testing commit from VS Code

--- calc_main/calc_main.go
+++ calc_main/calc_main.go
@@ -1,41 +1,42 @@
 package main
 
 import (
-    "fmt"
-    "calc"
-    "bufio"
-    "os"
-    "strings"
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+
+	"github.com/calc"
 )
 
-func main () {
-    reader := bufio.NewReader(os.Stdin)
-    result := 0
+func main() {
+	reader := bufio.NewReader(os.Stdin)
+	result := 0
 
-    fmt.Println("Welcome to Mike's first Go calculator!")
-    fmt.Println("This basic calculator supports addition, subtraction, multiplication, and division.")
+	fmt.Println("Welcome to Mike's first Go calculator!")
+	fmt.Println("This basic calculator supports addition, subtraction, multiplication, and division.")
 
-    for {
-        fmt.Print("\nWhat can I compute for you? (type 'exit' to stop): ")
-        formula, _ := reader.ReadString('\n')
+	for {
+		fmt.Print("\nWhat can I compute for you? (type 'exit' to stop): ")
+		formula, _ := reader.ReadString('\n')
 
-        if (strings.TrimSpace(strings.ToLower(formula)) == "exit") {
-            break;
-        }
+		if strings.TrimSpace(strings.ToLower(formula)) == "exit" {
+			break
+		}
 
-        formulaParts := calc.ParseFormula(formula)
+		formulaParts := calc.ParseFormula(formula)
 
-        switch formulaParts.Operator {
-            case "+":
-                result = calc.Add(formulaParts.Expression1, formulaParts.Expression2)
-            case "-":
-                result = calc.Subtract(formulaParts.Expression1, formulaParts.Expression2)
-            case "*":
-                result = calc.Multiply(formulaParts.Expression1, formulaParts.Expression2)
-            case "/":
-                result = calc.Divide(formulaParts.Expression1, formulaParts.Expression2)
-        }
+		switch formulaParts.Operator {
+		case "+":
+			result = calc.Add(formulaParts.Expression1, formulaParts.Expression2)
+		case "-":
+			result = calc.Subtract(formulaParts.Expression1, formulaParts.Expression2)
+		case "*":
+			result = calc.Multiply(formulaParts.Expression1, formulaParts.Expression2)
+		case "/":
+			result = calc.Divide(formulaParts.Expression1, formulaParts.Expression2)
+		}
 
-        fmt.Printf("The answer is %d\n", result)
-    }
+		fmt.Printf("The answer is %d\n", result)
+	}
 }
